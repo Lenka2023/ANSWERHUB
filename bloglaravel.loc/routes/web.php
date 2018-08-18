53c34768f9b35d1120a0a0d26c168aa671a33d3a
Elena <Lenka20231@gmail.com>
2018-08-18 23:34:06 +0300

    don't work second route

--- bloglaravel.loc/routes/web.php
+++ bloglaravel.loc/routes/web.php
@@ -14,13 +14,16 @@
 Route::get('/', function () {
 	
 	$posts=DB::table('posts')->get();
+	//dd($posts);
 	//return $posts;
    return view('index', compact('posts')  );
 });
 
-Auth::routes();
+//Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/posts/{id}',function($id){
-	dd($id);
+	$post=DB::table('posts')->find($id);
+	dd($post);
+	//dd($id);
 });
