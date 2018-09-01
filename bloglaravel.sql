68f180b339d6261a77441dced1ccb4a8066f19b1
Elena <Lenka20231@gmail.com>
2018-09-01 18:34:57 +0300

    add new dump for bloglaravel.loc

--- bloglaravel.sql
+++ bloglaravel.sql
@@ -1,22 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.7
--- https://www.phpmyadmin.net/
+-- version 4.2.11
+-- http://www.phpmyadmin.net
 --
--- Хост: 127.0.0.1:3306
--- Время создания: Авг 25 2018 г., 16:35
--- Версия сервера: 5.6.38
--- Версия PHP: 5.6.32
+-- Хост: 127.0.0.1
+-- Время создания: Сен 01 2018 г., 17:33
+-- Версия сервера: 5.6.21
+-- Версия PHP: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- База данных: `bloglaravel`
@@ -28,11 +26,11 @@ SET time_zone = "+00:00";
 -- Структура таблицы `migrations`
 --
 
-CREATE TABLE `migrations` (
-  `id` int(10) UNSIGNED NOT NULL,
+CREATE TABLE IF NOT EXISTS `migrations` (
+`id` int(10) unsigned NOT NULL,
   `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Дамп данных таблицы `migrations`
@@ -50,8 +48,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 -- Структура таблицы `myusers`
 --
 
-CREATE TABLE `myusers` (
-  `id` int(10) UNSIGNED NOT NULL,
+CREATE TABLE IF NOT EXISTS `myusers` (
+`id` int(10) unsigned NOT NULL,
   `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -66,7 +64,7 @@ CREATE TABLE `myusers` (
 -- Структура таблицы `password_resets`
 --
 
-CREATE TABLE `password_resets` (
+CREATE TABLE IF NOT EXISTS `password_resets` (
   `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
@@ -78,15 +76,15 @@ CREATE TABLE `password_resets` (
 -- Структура таблицы `posts`
 --
 
-CREATE TABLE `posts` (
-  `id` int(10) UNSIGNED NOT NULL,
+CREATE TABLE IF NOT EXISTS `posts` (
+`id` int(10) unsigned NOT NULL,
   `title` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `intro` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `body` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   `alias` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Дамп данных таблицы `posts`
@@ -96,11 +94,9 @@ INSERT INTO `posts` (`id`, `title`, `intro`, `body`, `created_at`, `updated_at`,
 (1, 'Php is awersome', 'Sed ut perspiciatis, unde omnis iste natus error ', ' Et harum quidem rerum facilis est et expedita distinctio.', NULL, NULL, 'php_is_awesome'),
 (2, 'Laravel 5.4', 'Sed ut perspiciatis, unde omnis iste natus error ', ' Et harum quidem rerum facilis est et expedita distinctio.', NULL, NULL, 'laravel54'),
 (3, 'Thank to seed', 'Sed ut perspiciatis, unde omnis iste natus error ', ' Et harum quidem rerum facilis est et expedita distinctio.', NULL, NULL, 'seeds'),
-(4, 'test', 'test', 'test', '2018-08-25 06:12:11', '2018-08-25 06:12:11', 'test'),
-(5, 'test', 'test', 'test', '2018-08-25 06:12:54', '2018-08-25 06:12:54', 'test'),
-(6, 'test', 'test', 'test', '2018-08-25 06:13:18', '2018-08-25 06:13:18', 'test'),
-(7, 'test', 'test', 'test', '2018-08-25 07:00:06', '2018-08-25 07:00:06', 'test'),
-(10, 'test123', 'test123', 'test123', '2018-08-25 09:29:11', '2018-08-25 09:42:58', 'test123');
+(6, 'test2', 'test2', 'test2', '2018-08-25 06:13:18', '2018-08-26 15:43:10', 'test2'),
+(10, 'test1234', 'test1234', 'test1234', '2018-08-25 09:29:11', '2018-08-26 15:44:04', 'test1234'),
+(12, 'test210', '0', '0', '2018-09-01 11:49:59', '2018-09-01 12:19:42', 'test21');
 
 --
 -- Индексы сохранённых таблиц
@@ -110,26 +106,25 @@ INSERT INTO `posts` (`id`, `title`, `intro`, `body`, `created_at`, `updated_at`,
 -- Индексы таблицы `migrations`
 --
 ALTER TABLE `migrations`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Индексы таблицы `myusers`
 --
 ALTER TABLE `myusers`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `myusers_email_unique` (`email`);
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `myusers_email_unique` (`email`);
 
 --
 -- Индексы таблицы `password_resets`
 --
 ALTER TABLE `password_resets`
-  ADD KEY `password_resets_email_index` (`email`);
+ ADD KEY `password_resets_email_index` (`email`);
 
 --
 -- Индексы таблицы `posts`
 --
 ALTER TABLE `posts`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
@@ -139,21 +134,17 @@ ALTER TABLE `posts`
 -- AUTO_INCREMENT для таблицы `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT для таблицы `myusers`
 --
 ALTER TABLE `myusers`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT для таблицы `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-COMMIT;
-
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
